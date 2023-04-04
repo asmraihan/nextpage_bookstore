ebeada33117134218542ba032aa12d692b1c66eb
asmraihan <asmraihanctg14@gmail.com>
2023-04-04 15:50:31 +0600

    home page addedt

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 const App = () => {
   return (
     <div>
-      {/* header */}
+      {/* navbar */}
+      <NavBar></NavBar>
       <Outlet/>
       {/* footer */}
     </div>
